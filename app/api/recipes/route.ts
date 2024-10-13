a99bf5c9eb59c558fdb0b8314058ab4cbb73d35e
Austen Tucker <austen@thearcades.me>
2024-10-13 17:41:02 -0500

    Fixed dummy route call

--- app/api/recipes/route.ts
+++ app/api/recipes/route.ts
@@ -1,9 +1,15 @@
 import { NextResponse } from 'next/server';
-import dummyData from '../../../data/dummyData.json';
+import { promises as fs } from 'fs';
+import path from 'path';
 
 export async function GET() {
   // Simulate API delay
   await new Promise(resolve => setTimeout(resolve, 1000));
+  
+  // Read the JSON file
+  const jsonDirectory = path.join(process.cwd(), 'data');
+  const fileContents = await fs.readFile(jsonDirectory + '/dummyData.json', 'utf8');
+  const dummyData = JSON.parse(fileContents);
   
   return NextResponse.json(dummyData);
 }
