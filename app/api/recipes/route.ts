df52c1cb5a316d748f1a2724d2043f4d46a457b3
Austen Tucker <austen@thearcades.me>
2024-10-13 17:45:26 -0500

    initial tailwind commit works!

--- app/api/recipes/route.ts
+++ app/api/recipes/route.ts
@@ -1,15 +1,9 @@
 import { NextResponse } from 'next/server';
-import { promises as fs } from 'fs';
-import path from 'path';
+import dummyData from '../../../../data/dummyData.json';
 
-export async function GET() {
+export async function GET(request: Request) {
   // Simulate API delay
   await new Promise(resolve => setTimeout(resolve, 1000));
-  
-  // Read the JSON file
-  const jsonDirectory = path.join(process.cwd(), 'data');
-  const fileContents = await fs.readFile(jsonDirectory + '/dummyData.json', 'utf8');
-  const dummyData = JSON.parse(fileContents);
   
   return NextResponse.json(dummyData);
 }
